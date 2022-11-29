3b56473a55c34f3577457fe5843aec45737a96f4
LI Tsz On <toli@sccpu1.cse.ust.hk>
2022-11-29 15:13:42 +0800

    remove main original

--- main.py
+++ main.py
@@ -22,24 +22,8 @@ labels_one_hot,molecules_features,edges,normalise_edges = preprocessing.preproce
 
 # inds = partition_train_val_test(smiles, dataset)
 train_partition, val_partition, test_partition = training.partition_dataset(smiles)
-# config_original = config.load_config("BBBP")
 model = training.build_gcn()
 
-####original
-
-# raw_data_original = utils.load_data(raw_data_file_path)
-# data_original = utils.preprocess(raw_data_original)
-# human_data_original = utils.load_human_data(config_original, "BBBP")
-# print(f"original train_dict: {human_data_original['train']}")
-# print(f"original test_dict: {human_data_original['test']}")
-# print(f"original val_dict: {human_data_original['val']}")
-# assert dict4train == human_data_original['train'], f"dict not equal {dict4train} vs {human_data_original['train']}"
-# assert dict4test == human_data_original['test'], f"dict not equal {dict4test} vs {human_data_original['test']}"
-# assert dict4val == human_data_original['val'], f"dict not equal {dict4val} vs {human_data_original['val']}"
-# inds_original = utils.partition_train_val_test(raw_data_original["smiles"], "BBBP")
-# train_inds_original = inds_original["train_inds"]
-# val_inds_original = inds_original["val_inds"]
-# test_inds_original = inds_original["test_inds"]
 config = config.load_config("BBBP")
 human_data = {"train": dict4train,
             "val": dict4val,
@@ -51,11 +35,6 @@ data = {'labels_one_hot': labels_one_hot,
             'norm_adjs': normalise_edges}
 model_out_fn = "GNES_{}.h5".format("BBBP".lower())
 save_path = os.path.join(config["saved_models_dir"], model_out_fn)
-# train_inds = train_partition
-# val_inds = val_partition
-
-# model_original = utils.keras_gcn(config)
-# model = utils.build_gcn(config)
 loss = utils.gcn_train(model, data, config['num_epochs'], train_partition, val_partition, save_path, human_data)
 
 # model = keras_gcn(training.build_gcn(config_original))
@@ -72,4 +51,3 @@ for metric in ["roc_auc", "avg_precision", "node_mse", "node_mae", "edge_mse", "
     for split in ["train", "val  ", "test "]:
         res = utils.print_eval_avg(eval_metric, split.strip(), metric)
         print(split + " " + res)
-    # print()
